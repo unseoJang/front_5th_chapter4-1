83d2f0fb760e4c28a17da296d1cae4b12fe37795
unseoJang <qqbck123@naver.com>
2025-05-27 14:53:02 +0900

    upload: 테스트용 이미지 업로드

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export",
+	images: {
+		unoptimized: true, // ✅ 정적 export에서 이미지 최적화 끔
+	},
 }
 
 export default nextConfig
